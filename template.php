0fca78af5ab76d4b978a1af0183455110a94a95b
nori122 <norito.sasaki.jp@gmail.com>
2020-07-03 16:02:42 +0900

    initial commit

---
+++ template.php
@@ -0,0 +1,32 @@
+<!---------------------
+php 要素
+--------------------->
+<?PHP
+
+
+
+?>
+
+
+
+<!---------------------
+HTML 要素
+--------------------->
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+    <meta charset='UTF-8'>
+    <link rel='stylesheet' href='styles.css'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.13.0/css/all.css' integrity='sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V' crossorigin='anonymous'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+    <title>Narikiri_login</title>
+</head>
+
+<body>
+    test
+
+</body>
+
+</html>
