bfe9e81119163c21c5215843f670bba1d179e7a3
nori122 <norito.sasaki.jp@gmail.com>
2020-07-08 19:24:15 +0900

    dbname_changed

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function connect_to_db()
 {
   // DB接続の設定
   // DB名は`gsacf_x00_00`にする
-  $dbn = 'mysql:dbname=Twitter;charset=utf8;port=3306;host=localhost';
+  $dbn = 'mysql:dbname=e0f52b3e9d761d71eaea871117239953;charset=utf8;port=3306;host=localhost';
   $user = 'root';
   $pwd = '';
 
